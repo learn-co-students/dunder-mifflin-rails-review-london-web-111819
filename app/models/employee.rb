60932388ddd96b0aba2fb52a708c647ab21b7932
Ali <saida130.210@gmail.com>
2019-12-18 11:41:19 +0000

    Done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,10 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+    validates :alias, uniqueness: {message: "must be unique"}
+    validates :title, uniqueness: {message: "must be unique"}
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+    
 end
